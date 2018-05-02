d6fe066bb1ca2743c483096a43481f2cff8edb7c
Kinza Rian <rian.kinza@gmail.com>
2018-05-02 18:41:30 +0200

    Update config.js

--- metabolizer-web-master/conf/config.js
+++ metabolizer-web-master/conf/config.js
@@ -5,7 +5,8 @@
 STEVIA_MANAGER_LOG = true;
 // STEVIA_SERVER_HOST = 'http://localhost:5555';
 // STEVIA_SERVER_HOST = 'http://ws.babelomics.org/teststv01';
-STEVIA_SERVER_HOST = 'http://ws.babelomics.org/stv01';
+//STEVIA_SERVER_HOST = 'http://ws.babelomics.org/stv01';
+STEVIA_SERVER_HOST = 'http://ws2.babelomics.org/stevia_cellmaps_hipathia_pathact_metabolizer';
 
 STEVIA_SHOW_JOB_ERROR_FILES = true;
 UPLOAD_FILES_WITH_ERRORS = true;
