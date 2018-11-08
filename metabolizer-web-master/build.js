70e5fb5b81f69a17e8a1e756fc865b303f99f7ca
kinzaR <rian.kinza@gmail.com>
2018-11-08 18:27:50 +0100

    stv->elm and stevia->cba

--- metabolizer-web-master/build.js
+++ metabolizer-web-master/build.js
@@ -48,14 +48,14 @@ async.waterfall([
         shell.cp('-r', indexHTML, buildIndexHTML);
         shell.cp('-r', path.join(__dirname, 'conf/'), bp);
         shell.cp('-r', path.join(__dirname, 'images/'), bp);
-        shell.cp('-r', path.join(__dirname, 'bower_components', 'stevia-elements', 'fonts'), bp);
-        shell.cp('-r', path.join(__dirname, 'bower_components', 'stevia-elements', 'css'), bp);
+        shell.cp('-r', path.join(__dirname, 'bower_components', 'cba-elements', 'fonts'), bp);
+        shell.cp('-r', path.join(__dirname, 'bower_components', 'cba-elements', 'css'), bp);
         shell.cp('-r', path.join(__dirname, 'bower_components', 'fontawesome', 'css'), path.join(bp, "fontawesome/"));
         shell.cp('-r', path.join(__dirname, 'bower_components', 'fontawesome', 'fonts'), path.join(bp, "fontawesome/"));
         shell.cp('-r', path.join(__dirname, 'bower_components', 'webcomponentsjs', '*.min.js'), path.join(bp, "webcomponentsjs/"));
 
         // fix index.html paths
-        shell.sed('-i', 'bower_components/stevia-elements/', '', buildIndexHTML);
+        shell.sed('-i', 'bower_components/cba-elements/', '', buildIndexHTML);
         shell.sed('-i', 'bower_components/', '', buildIndexHTML);
 
         cb(null);
--- metabolizer-web-master/conf/config.js
+++ metabolizer-web-master/conf/config.js
@@ -2,12 +2,13 @@
  * This is a configuration file.
  * Changes to this file may cause the application does not work as it should
  **/
-STEVIA_MANAGER_LOG = true;
-// STEVIA_SERVER_HOST = 'http://localhost:5555';
-// STEVIA_SERVER_HOST = 'http://ws.babelomics.org/teststv01';
-//STEVIA_SERVER_HOST = 'http://ws.babelomics.org/stv01';
-STEVIA_SERVER_HOST = 'http://ws2.babelomics.org/stevia_cellmaps_hipathia_pathact_metabolizer';
+//CBA_MANAGER_LOG = true;
+// CBA_SERVER_HOST = 'http://localhost:5555';
+// CBA_SERVER_HOST = 'http://ws.babelomics.org/teststv01';
+//CBA_SERVER_HOST = 'http://ws.babelomics.org/stv01';
+//CBA_SERVER_HOST = 'http://ws2.babelomics.org/stevia_cellmaps_hipathia_pathact_metabolizer';
+CBA_SERVER_HOST ='http://stevia-curso.clinbioinfosspa.es/slurmia01';
 
-STEVIA_SHOW_JOB_ERROR_FILES = true;
-UPLOAD_FILES_WITH_ERRORS = true;
-STEVIA_SHOW_HIDDEN_FILES = true;
+CBA_SHOW_JOB_ERROR_FILES = false;
+UPLOAD_FILES_WITH_ERRORS = false;
+CBA_SHOW_HIDDEN_FILES = false;
