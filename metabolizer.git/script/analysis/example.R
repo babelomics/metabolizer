2b7de2255a00bd6114fdf33daac99ea71493d019
cankutcubuk <cankutcubuk@gmail.com>
2019-03-19 04:39:27 +0000

    Metabolizer R script
    Here you can find the R script of the Metabolizer tool and some toy examples of its usage. 
    As well as using this R code, you can also perform the analysis of the metabolic module activity using the Metabolizer webserver at: http://metabolizer.babelomics.org/
    
    We will be adding more species to our tool. You will be able to follow or find the upcoming updates in this github repository.
    
    Please, use the following convention to cite Metabolizer tool:
    Cubuk, C., Hidalgo, M., Amadoz, A., Rian, K., Salavert, F., Pujana, M., Mateo, F., Herranz, C., Carbonell-Caballero, J. and Dopazo, J. (2019). Differential metabolic activity and discovery of therapeutic targets using summarized metabolic pathway models. npj Systems Biology and Applications, 5(1), doi: 10.1038/s41540-019-0087-2.

---
+++ metabolizer.git/script/analysis/example.R
@@ -0,0 +1,66 @@
+############################################################ 
+#         Examples for how to run the Metabolizer          #
+#                                                          #
+#         cankutcubuk [at] {gmail} [dot] {com}             #
+#                     2016-2019                            #
+#             @ Sevilla and Valencia, Spain                #
+############################################################ 
+
+
+
+##################################################
+#             Example of Homo sapiens            #
+##################################################
+
+
+rm(list=ls())
+
+source("./metabolizer.git/script/utils/functions.main.14032019.R")
+load("./metabolizer.git/data//KEGGfiles/hsa/hsa_module_data_March2019.RData")
+
+### just to create a fake gene expression file: an example input file ###
+# KEGG or NCBI (Entrez) gene ids are used in the Metabolizer tool. Check which one is used for your model organism
+# You can use xxx_geneIDs.RData file to convert your gene symbols into gene ids that are required by the Metabolizer.
+  all_module_genes_vec <- get.All.module.genes(hsa_module_data)
+# simulate some missing genes
+  all_module_genes_vec <- all_module_genes_vec[-sample(1:length(all_module_genes_vec),40)] 
+  fakeExp <- matrix(data=rexp(length(all_module_genes_vec)*20, rate = 0.1), nrow = length(all_module_genes_vec), ncol = 20)
+  dimnames(fakeExp) <- list(all_module_genes_vec, LETTERS[1:20])
+### end of fake gene expression file ###
+
+results <- metabolizer(hsa_module_data, 
+                       combat.vals=fakeExp, 
+                       output_folder=NULL, saveName=NULL, 
+                       onesample=F, 
+                       expbased=T, fluxbased=F, 
+                       default_value=0.5, 
+                       moduleinfo="./metabolizer.git/data/KEGGfiles/hsa/hsa_moduleinfo.RData")
+
+
+##################################################
+# Example of Drosophila melanogaster (fruit fly) #
+##################################################
+
+rm(list=ls())
+
+source("./metabolizer.git/script/utils/functions.main.14032019.R")
+load("./metabolizer.git/data//KEGGfiles/dme/dme_module_data_March2019.RData")
+
+### just to create a fake gene expression file: an example input file ###
+# KEGG or NCBI (Entrez) gene ids are used in the Metabolizer tool. Check which one is used for your model organism
+# You can use xxx_geneIDs.RData file to convert your gene symbols into gene ids that are required by the Metabolizer.
+all_module_genes_vec <- get.All.module.genes(hsa_module_data)
+# simulate some missing genes
+all_module_genes_vec <- all_module_genes_vec[-sample(1:length(all_module_genes_vec),40)] 
+fakeExp <- matrix(data=rexp(length(all_module_genes_vec)*20, rate = 0.1), nrow = length(all_module_genes_vec), ncol = 20)
+dimnames(fakeExp) <- list(all_module_genes_vec, LETTERS[1:20])
+### end of fake gene expression file ###
+
+results <- metabolizer(hsa_module_data, 
+                       combat.vals=fakeExp, 
+                       output_folder=NULL, saveName=NULL, 
+                       onesample=F, 
+                       expbased=T, fluxbased=F, 
+                       default_value=0.5, 
+                       moduleinfo="./metabolizer.git/data/KEGGfiles/dme/dme_moduleinfo.RData")
+
